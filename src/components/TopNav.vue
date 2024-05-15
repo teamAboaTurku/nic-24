3a3cb834adb54f5cbee7e06609df4b91dcc3e8c2
elvleh <elvleh@utu.fi>
2024-05-15 12:22:45 +0300

    add logo to top nav

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="topnav">
-    <router-link to="/" exact class="nav-item" active-class="active">Home</router-link>
-    <router-link to="/timetable" class="nav-item" active-class="active">Timetable</router-link>
-    <router-link to="/map" class="nav-item" active-class="active">Map</router-link>
-    <router-link to="/about" class="nav-item" active-class="active">About NiC</router-link>
+    <div class="topnavLogo">
+      <img :src="require('@/assets/photos/nic_logo.png')" alt="Logo" id="Logo">
+    </div>
+    <div class="topnavLinks">
+      <router-link to="/contacts" class="nav-item" active-class="active">Contact us</router-link>
+      <router-link to="/map" class="nav-item" active-class="active">Map</router-link>
+      <router-link to="/timetable" class="nav-item" active-class="active">Timetable</router-link>
+      <router-link to="/about" class="nav-item" active-class="active">About</router-link>
+      <router-link to="/" exact class="nav-item" active-class="active">Home</router-link>
+    </div>
   </div>
 </template>
 
@@ -12,4 +18,3 @@ export default {
   name: 'TopNav'
 }
 </script>
-
