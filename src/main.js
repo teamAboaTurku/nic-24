adc904e09ad338e2cb64f3caf425cef54b55fe33
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-21 16:51:09 +0300

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/styles.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const app = createApp(App);
 
