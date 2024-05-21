bc97376816eb46909c352365748d23386937730a
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-21 18:11:11 +0300

    import leaflet

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import './assets/styles.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'leaflet/dist/leaflet.css';
 
 const app = createApp(App);
 
