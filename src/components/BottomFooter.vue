e1467927bbca0e3f3c57b8c784b949d3cd38ee04
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-21 17:42:34 +0300

    fixed footer

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,11 +1,25 @@
 <template>
     <footer>
-        <p>Footer</p>
+        <SponsorContainer />
+        <SomeBox />
     </footer>
 </template>
 
 <script>
+import SponsorContainer from './SponsorContainer.vue'
+import SomeBox from './SomeBox.vue';
+
 export default {
-    name: 'BottomFooter'
+    name: 'BottomFooter',
+    components: {
+        SponsorContainer,
+        SomeBox
+    }
 }
 </script>
+
+<style>
+footer div.someBox a.some_icon{
+    color: rgba(255, 255, 255, 0.5);
+}
+</style>
