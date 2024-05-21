03f36f2a9fd1316ef216de4696f90b517b991ba2
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-21 15:12:22 +0300

    make circles for home page content

---
+++ src/components/CircleContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section style="height: 100px;"></section>
+    <div class="circle-wrapper">
+      <div class="circle-container">
+        <div class="circle">
+          <slot name="content1"></slot>
+        </div>
+      </div>
+    </div>
+    <div class="circle-wrapper">
+      <div class="circle-container">
+        <div class="circle">
+          <slot name="content2"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CircleContent'
+};
+</script>
+
+<style>
+/* Include the refined styles here */
+</style>
