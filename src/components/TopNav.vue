8306bd5db8b6ff97dfdcbe1564718f2599e0fa83
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-21 15:11:49 +0300

    deleted links to removed about page

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -7,7 +7,6 @@
       <router-link to="/contacts" class="nav-item" active-class="active">Contact us</router-link>
       <router-link to="/map" class="nav-item" active-class="active">Map</router-link>
       <router-link to="/timetable" class="nav-item" active-class="active">Timetable</router-link>
-      <router-link to="/about" class="nav-item" active-class="active">About</router-link>
       <router-link to="/" exact class="nav-item" active-class="active">Home</router-link>
     </div>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
-import AboutPage from '../pages/AboutPage.vue';
 import TimetablePage from '../pages/TimetablePage.vue';
 import MapPage from '../pages/MapPage.vue';
 import ContactsPage from '../pages/ContactsPage.vue';
@@ -9,7 +8,6 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage, name: 'HomePage' },
-    { path: '/about', component: AboutPage, name: 'AboutPage' },
     { path: '/timetable', component: TimetablePage, name: 'TimetablePage' },
     { path: '/map', component: MapPage, name: 'MapPage' },
     { path: '/contacts', component: ContactsPage, name: 'ContactsPage' },
