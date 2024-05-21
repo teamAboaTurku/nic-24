52c6c4db90b575186703b9c5025f6ff846abd13b
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-21 15:12:09 +0300

    home page content template

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,34 @@
 <template>
-    <h1>Nordic iGem Conference 2024</h1>
+  <div class="page">
+    <CircleContent>
+      <template #content1>
+        <div>
+          <h2>What is Nic?</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quidem eos, aut voluptate eveniet
+            molestias ipsa asperiores fuga officia dolor nesciunt. Dolor illo quidem, impedit doloremque sed nostrum?
+            Cumque, nesciunt.
+          </p>
+        </div>
+      </template>
+      <template #content2>
+        <div>
+          <h2>When and where?</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, minus sit velit vero fugiat quibusdam
+            consequuntur natus amet maxime animi nobis delectus eos? Suscipit quidem, amet aperiam inventore quo id?
+          </p>
+        </div>
+      </template>
+    </CircleContent>
+  </div>
 </template>
 
 <script>
+import CircleContent from '../components/CircleContent.vue';
+
 export default {
   name: 'HomePage',
+  components: {
+    CircleContent
+  }
 };
 </script>
