e677cf85ef1bef133a580ef6b806bab3c307be7a
elvleh <elvleh@utu.fi>
2024-05-24 14:10:12 +0300

    remove tests

--- src/components/SponsorContainer.vue
+++ src/components/SponsorContainer.vue
@@ -3,12 +3,6 @@
         <h3>We would like to thank our sponsors:</h3>
         <div class="sponsor-list">
             <div class="sponsor">
-                <img src="https://static.igem.wiki/teams/4831/wiki/logos/utu-logo.jpg" alt="University of Turku logo">
-            </div>
-            <div class="sponsor">
-                <img src="https://static.igem.wiki/teams/4831/wiki/logos/turku-amk-eng-logo.png" alt="Turku AMK logo">
-            </div>
-            <div class="sponsor">
                 <img src="https://static.igem.wiki/teams/4831/wiki/logos/a-logo.png" alt="Åbo Akademi logo">
             </div>
         </div>
