a95650c22239bc23f3e18edd1434fb94ade5e1b6
elvleh <elvleh@utu.fi>
2024-05-27 17:23:24 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/nic_24/'
-    : '/'
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/nic_24/' : '/'
+});
