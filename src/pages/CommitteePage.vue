0722f5acd0f0a7b1d899ab87d32bca5a24f5ddf7
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-06-06 14:10:19 +0300

    add photocards into committee page and note

--- src/pages/CommitteePage.vue
+++ src/pages/CommitteePage.vue
@@ -1,16 +1,107 @@
 <template>
-    <div class="page">
-        <h1>NiC 2024 Committee</h1>
-        <h3>The powerhouse team of the NiC committee, orchestrating every detail for the upcoming Nordic iGEM Conference.</h3>
+    <h1>NiC 2024 Committee</h1>
+    <h3>The powerhouse team of the NiC committee, orchestrating every detail for the upcoming Nordic iGEM Conference.</h3>
+    <h3 style="color: var(--red);">Note: This site is under construction!</h3>
+    <div class="grid-container">
+        <PhotoCard v-for="(item, index) in items" :key="index" :name="item.name" :year="item.year" :imgUrl="item.imgUrl"
+            :class="`grid-item`" />
     </div>
 </template>
 
 <script>
+import PhotoCard from '../components/PhotoCard.vue';
+
 export default {
-    name: 'CommitteePage'
+    components: {
+        PhotoCard
+    },
+    data() {
+        return {
+            items: [
+                { name: 'Mariel', year: '2023', imgUrl: 'map.png' },
+                { name: 'Sara', year: '2023', imgUrl: 'map.png' },
+                { name: 'Minttu', year: '2023', imgUrl: 'map.png' },
+                { name: 'Ville', year: '2023', imgUrl: 'map.png' },
+                { name: 'Ofelia', year: '2024', imgUrl: 'map.png' },
+                { name: 'Elsa', year: '2023', imgUrl: 'map.png' },
+                { name: 'Jesse', year: '2023', imgUrl: 'map.png' }
+            ]
+        };
+    }
 }
 </script>
 
 <style>
+.grid-container {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 10px;
+}
 
+.grid-item:nth-child(1),
+.grid-item:nth-child(2) {
+    grid-column: span 3;
+}
+
+.grid-item:nth-child(3),
+.grid-item:nth-child(4),
+.grid-item:nth-child(5) {
+    grid-column: span 2;
+}
+
+.grid-item:nth-child(6),
+.grid-item:nth-child(7) {
+    grid-column: span 3;
+}
+
+.grid-item:nth-child(1),
+.grid-item:nth-child(6) {
+    grid-column: 2 / span 2;
+}
+
+.grid-item:nth-child(2),
+.grid-item:nth-child(7) {
+    grid-column: 4 / span 2;
+}
+
+.grid-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+@media only screen and (min-width: 430px) and (max-width: 700px) {
+    .grid-container {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    .grid-item:nth-child(1),
+    .grid-item:nth-child(2),
+    .grid-item:nth-child(3),
+    .grid-item:nth-child(4),
+    .grid-item:nth-child(5),
+    .grid-item:nth-child(6) {
+        grid-column: span 1;
+    }
+
+    .grid-item:nth-child(7) {
+        grid-column: span 2;
+    }
+}
+
+@media only screen and (max-width: 425px) {
+    .grid-container {
+        grid-template-columns: repeat(1, 1fr);
+    }
+
+    .grid-item:nth-child(1),
+    .grid-item:nth-child(2),
+    .grid-item:nth-child(3),
+    .grid-item:nth-child(4),
+    .grid-item:nth-child(5),
+    .grid-item:nth-child(6),
+    .grid-item:nth-child(7) {
+        grid-column: span 1;
+    }
+}
 </style>
