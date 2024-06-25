6851df1521de7ad9879d1cb7c9e7af6fbca7eaec
elvleh <elvleh@utu.fi>
2024-06-25 10:52:57 +0300

    add local emergency numbers

--- src/components/ContactsBox.vue
+++ src/components/ContactsBox.vue
@@ -4,7 +4,15 @@
         <p>team@aboaturku.fi</p>
         <h3>or via social media:</h3>
         <SomeBox />
-        <h3>Details of the main contact person for the event will be posted here closer to the event date.</h3>
+        <hr style="background-color: var(--blue); height: 5px;">
+        <h1>During event</h1>
+        <p>Details of the main contact person for the event will be posted here closer to the event date. Partisipants
+            can also contact organizers through telegram.</p>
+        <p>In case of an emergency, the local emergency number is <span style="font-weight: bold;">112</span>.</p>
+        <p>The local medical help line is <span style="font-weight: bold;">116117</span>.
+            Emergency services are open 24/7 and their contact info and instructions can be read <a
+                href="https://www.tyks.fi/en/patients-and-visitors/hospitals-and-units/tyks-t-hospital/emergency-services-turku-tyks-t-hospital">here</a>.
+        </p>
     </div>
 </template>
 
@@ -24,10 +32,11 @@ export default {
     margin: auto;
     width: 90%;
     min-width: 200px;
-    max-width: 400px;
+    max-width: 600px;
     text-align: center;
 }
-.contactsBox .someBox a.some_icon{
+
+.contactsBox .someBox a.some_icon {
     color: var(--green);
 }
 </style>
