be75d3f66efb0f3fddd3b28131cf9b8636baf604
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-06-26 16:55:41 +0300

    change to hahs mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import TimetablePage from '../pages/TimetablePage.vue';
 import MapPage from '../pages/MapPage.vue';
@@ -7,14 +7,14 @@ import CommitteePage from '../pages/CommitteePage.vue'
 import ContactsPage from '../pages/ContactsPage.vue';
 
 const router = createRouter({
-  history: createWebHistory('/nic_24/'),
+  history: createWebHashHistory('/nic_24/'),
   routes: [
-    { path: '/', component: HomePage, name: 'HomePage', meta: { bannerClass: 'home-banner' }},
-    { path: '/timetable', component: TimetablePage, name: 'TimetablePage', meta: { bannerClass: 'timetable-banner' }},
+    { path: '/', component: HomePage, name: 'HomePage', meta: { bannerClass: 'home-banner' } },
+    { path: '/timetable', component: TimetablePage, name: 'TimetablePage', meta: { bannerClass: 'timetable-banner' } },
     { path: '/map', component: MapPage, name: 'MapPage', meta: { bannerClass: 'map-banner' } },
-    { path: '/conference', component: ConferencePage, name: 'ConferencePage', meta: { bannerClass: 'conference-banner'} },
-    { path: '/committee', component: CommitteePage, name: 'CommitteePage', meta: { bannerClass: 'committee-banner'}},
-    { path: '/contacts', component: ContactsPage, name: 'ContactsPage', meta: { bannerClass: 'contacts-banner' }},
+    { path: '/conference', component: ConferencePage, name: 'ConferencePage', meta: { bannerClass: 'conference-banner' } },
+    { path: '/committee', component: CommitteePage, name: 'CommitteePage', meta: { bannerClass: 'committee-banner' } },
+    { path: '/contacts', component: ContactsPage, name: 'ContactsPage', meta: { bannerClass: 'contacts-banner' } },
     // other routes
   ],
 });
